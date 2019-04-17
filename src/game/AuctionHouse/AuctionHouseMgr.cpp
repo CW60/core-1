d9dd1e4b94c64f891cc26bfe49def86632a5ab50
蔡忠振 <zhongzhen.cai@five-star.cn>
2019-04-17 09:46:46 +0800

    回滚原版拍卖代码

--- src/game/AuctionHouse/AuctionHouseMgr.cpp
+++ src/game/AuctionHouse/AuctionHouseMgr.cpp
@@ -662,7 +662,7 @@ void AuctionHouseObject::Update()
 
         next = itr;
         ++next;
-        if (curTime > entry->expireTime)
+        if (curTime > (entry->expireTime))
         {
             ///- Either cancel the auction if there was no bidder
             if (entry->bidder == 0)
